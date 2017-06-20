c56f2b51bebdae1462b85a50ffeaa934b9c9ac7f
rnkhouse <rnkhouse@yahoo.com>
2017-06-19 21:38:15 -0400

    fixed errors

--- ui/lib/explorer-link.js
+++ ui/lib/explorer-link.js
@@ -5,13 +5,8 @@ module.exports = function (hash, network) {
     case 88: // main net
       prefix = 'rpc1.'
       break
-<<<<<<< HEAD
     case 9: // rinkeby test net
       prefix = 'rinkeby.'
-=======
-    case 9: // test net
-      return `http://rinkeby.ubiqscan.io/tx/${hash}`
->>>>>>> 96af372859225b4c40a164d7483ed481f2e5862f
       break
     default:
       prefix = 'rpc1.'
