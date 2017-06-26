953fac575266d3a0b949af94f80878869e1ffb51
Aldo Borrero González <aldo@aldoborrero.com>
2017-06-26 19:56:49 +0200

    Fixed not passing tests

--- test/unit/account-link-test.js
+++ test/unit/account-link-test.js
@@ -4,13 +4,13 @@ var linkGen = require('../../ui/lib/account-link')
 describe('account-link', function () {
   it('adds rpc1 prefix to main network', function () {
     var result = linkGen('account', '88')
-    assert.notEqual(result.indexOf('rpc1'), -1, 'rpc1 included')
+    assert.notEqual(result.indexOf('ubiqscan.io'), -1, 'main network included')
     assert.notEqual(result.indexOf('account'), -1, 'account included')
   })
 
   it('adds rinkeby prefix to rinkeby test network', function () {
     var result = linkGen('account', '9')
-    assert.notEqual(result.indexOf('rinkeby'), -1, 'rinkeby included')
+    assert.notEqual(result.indexOf('rinkeby.ubiqscan'), -1, 'test network included')
     assert.notEqual(result.indexOf('account'), -1, 'account included')
   })
 })
--- test/unit/explorer-link-test.js
+++ test/unit/explorer-link-test.js
@@ -4,11 +4,11 @@ var linkGen = require('../../ui/lib/explorer-link')
 describe('explorer-link', function () {
   it('adds rpc1 prefix to main network', function () {
     var result = linkGen('hash', '88')
-    assert.notEqual(result.indexOf('rpc1'), -1, 'rpc1 injected')
+    assert.notEqual(result.indexOf('ubiqscan'), -1, 'main network injected')
   })
 
-  it('adds rinkeby prefix to rinkeby test network', function () {
+  it('adds rinkeby prefix to test network', function () {
     var result = linkGen('hash', '9')
-    assert.notEqual(result.indexOf('rinkeby'), -1, 'rinkeby injected')
+    assert.notEqual(result.indexOf('rinkeby'), -1, 'test network injected')
   })
 })
