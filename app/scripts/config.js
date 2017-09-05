8ac1d0adfd59da7ee0e3552f2aa7adef5acdcbdc
Luke Williams <luke@iquidus.io>
2017-09-05 08:35:21 +0200

    update mainnet node url

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,4 +1,4 @@
-const MAINET_RPC_URL = 'https://rpc1.ubiqscan.io'
+const MAINET_RPC_URL = 'https://sparrow1.ubiqscan.io'
 const TESTNET_RPC_URL = 'https://testrpc1.ubiqscan.io'
 
 global.METAMASK_DEBUG = 'GULP_METAMASK_DEBUG'
