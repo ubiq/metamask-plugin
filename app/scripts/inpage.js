f15126160bf216f1afd4c87713fd8e7c2f09433b
Aldo Borrero González <aldo@aldoborrero.com>
2017-10-24 21:39:02 +0200

    Added dirty fix to inpage.js to avoid having problems with web3 when using MetaMask plugin at the same time

--- app/scripts/inpage.js
+++ app/scripts/inpage.js
@@ -1,55 +1,58 @@
 /*global Web3*/
-cleanContextForImports()
-require('web3/dist/web3.min.js')
-const LocalMessageDuplexStream = require('post-message-stream')
-// const PingStream = require('ping-pong-stream/ping')
-// const endOfStream = require('end-of-stream')
-const setupDappAutoReload = require('./lib/auto-reload.js')
-const MetamaskInpageProvider = require('./lib/inpage-provider.js')
-restoreContextAfterImports()
+
+if (typeof global.web3 === 'undefined') {
+  cleanContextForImports()
+  require('web3/dist/web3.min.js')
+  const LocalMessageDuplexStream = require('post-message-stream')
+  // const PingStream = require('ping-pong-stream/ping')
+  // const endOfStream = require('end-of-stream')
+  const setupDappAutoReload = require('./lib/auto-reload.js')
+  const MetamaskInpageProvider = require('./lib/inpage-provider.js')
+  restoreContextAfterImports()
 
 
-//
-// setup plugin communication
-//
+  //
+  // setup plugin communication
+  //
 
-// setup background connection
-var metamaskStream = new LocalMessageDuplexStream({
-  name: 'inpage',
-  target: 'contentscript',
-})
+  // setup background connection
+  var metamaskStream = new LocalMessageDuplexStream({
+    name: 'inpage',
+    target: 'contentscript',
+  })
 
-// compose the inpage provider
-var inpageProvider = new MetamaskInpageProvider(metamaskStream)
+  // compose the inpage provider
+  var inpageProvider = new MetamaskInpageProvider(metamaskStream)
 
-//
-// setup web3
-//
+  //
+  // setup web3
+  //
 
-var web3 = new Web3(inpageProvider)
-web3.setProvider = function () {
-  console.log('MetaMask - overrode web3.setProvider')
+  var web3 = new Web3(inpageProvider)
+  web3.setProvider = function () {
+    console.log('MetaMask - overrode web3.setProvider')
+  }
+  console.log('MetaMask - injected web3')
+  // export global web3, with usage-detection
+  setupDappAutoReload(web3, inpageProvider.publicConfigStore)
+
+  // set web3 defaultAccount
+
+  inpageProvider.publicConfigStore.subscribe(function (state) {
+    web3.eth.defaultAccount = state.selectedAddress
+  })
+
+  //
+  // util
+  //
+
+  // need to make sure we aren't affected by overlapping namespaces
+  // and that we dont affect the app with our namespace
+  // mostly a fix for web3's BigNumber if AMD's "define" is defined...
+  var __define
 }
-console.log('MetaMask - injected web3')
-// export global web3, with usage-detection
-setupDappAutoReload(web3, inpageProvider.publicConfigStore)
 
-// set web3 defaultAccount
-
-inpageProvider.publicConfigStore.subscribe(function (state) {
-  web3.eth.defaultAccount = state.selectedAddress
-})
-
-//
-// util
-//
-
-// need to make sure we aren't affected by overlapping namespaces
-// and that we dont affect the app with our namespace
-// mostly a fix for web3's BigNumber if AMD's "define" is defined...
-var __define
-
-function cleanContextForImports () {
+function cleanContextForImports() {
   __define = global.define
   try {
     global.define = undefined
@@ -58,11 +61,10 @@ function cleanContextForImports () {
   }
 }
 
-function restoreContextAfterImports () {
+function restoreContextAfterImports() {
   try {
     global.define = __define
   } catch (_) {
     console.warn('MetaMask - global.define could not be overwritten.')
   }
 }
-
