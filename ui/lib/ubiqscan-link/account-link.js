694434aad6a295189fb1312a62cce37bbd68e04d
Aldo Borrero González <aldo@aldoborrero.com>
2018-01-19 01:47:09 +0100

    Removed option prefix for network (as currently ubiqscan does not support it properly)

--- ui/lib/ubiqscan-link/account-link.js
+++ ui/lib/ubiqscan-link/account-link.js
@@ -1,6 +1,3 @@
-const prefixForNetwork = require('./prefix-for-network')
-
 module.exports = function (address, network) {
-  const prefix = prefixForNetwork(network)
-  return `http://ubiqscan.io/address/${address}?node=${prefix}`
+  return `http://ubiqscan.io/address/${address}`
 }
